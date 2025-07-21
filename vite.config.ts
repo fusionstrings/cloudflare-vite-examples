1c6fc8ca531235b8858251de63818d91b0768b56
Dilip Kr. Shukla <fusionstrings@gmail.com>
2025-07-21 18:08:44 +0200

    feat: add internationalization support with language files and update settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js'
 import { defineConfig } from "vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-    plugins: [cloudflare()],
+    plugins: [paraglideVitePlugin({ project: './project.inlang', outdir: './i18n/paraglide' }),cloudflare()],
     "build": {
         sourcemap: true,
         manifest: true,
